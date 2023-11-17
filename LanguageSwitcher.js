320afc96e04dc0dc313f39a5d1f1697a1e701885
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-11-17 15:47:57 +0100

    Language selector updating

--- LanguageSwitcher.js
+++ LanguageSwitcher.js
@@ -1,15 +1,21 @@
 document.getElementById('languageSelector').addEventListener('change', function () {
-  var language = this.value;
-  switch (language) {
-    case 'en':
-        window.location.href = 'https://en.einfachwebsiteerstellen.de/'; // 设置为根目录下的 en 文件夹中的 index_en.html
-        break;
-    case 'de':
-        window.location.href = 'https://einfachwebsiteerstellen.de/'; // 替换为德语页面的实际 URL
-        break;
-    case 'cn':
-        window.location.href = 'https://cn.einfachwebsiteerstellen.de/'; // 替换为中文页面的实际 URL
-        break;
-    // 可以根据需要添加更多的 case
-}
+    var language = this.value;
+    var currentUrl = new URL(window.location.href);
+
+    // 根据选择的语言更改子域名
+    switch (language) {
+        case 'en':
+            currentUrl.hostname = 'en.einfachwebsiteerstellen.de';
+            break;
+        case 'de':
+            currentUrl.hostname = 'einfachwebsiteerstellen.de';
+            break;
+        case 'cn':
+            currentUrl.hostname = 'cn.einfachwebsiteerstellen.de';
+            break;
+        // 可以根据需要添加更多的 case
+    }
+
+    // 跳转到新的URL
+    window.location.href = currentUrl.href;
 });
