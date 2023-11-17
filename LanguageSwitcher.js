70edfc671d820f8a21c3cf92b36181d9a73259b9
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-11-17 15:53:42 +0100

    Language Selector Updating

--- LanguageSwitcher.js
+++ LanguageSwitcher.js
@@ -2,6 +2,18 @@ document.getElementById('languageSelector').addEventListener('change', function 
     var language = this.value;
     var currentUrl = new URL(window.location.href);
 
+    // 获取当前的路径（例如 "/c/page.html"）
+    var currentPath = currentUrl.pathname;
+
+    // 假设您的URL结构是这样的：
+    // 英文: https://en.einfachwebsiteerstellen.de/c/page.html
+    // 德语: https://einfachwebsiteerstellen.de/c/page.html
+    // 中文: https://cn.einfachwebsiteerstellen.de/c/page.html
+
+    // 移除语言部分（如果有）来获取基础路径
+    // 例如，从 "/en/c/page.html" 获取 "/c/page.html"
+    var basePath = currentPath.replace(/^\/(en|de|cn)\//, '/');
+
     // 根据选择的语言更改子域名
     switch (language) {
         case 'en':
@@ -15,6 +27,8 @@ document.getElementById('languageSelector').addEventListener('change', function 
             break;
         // 可以根据需要添加更多的 case
     }
+
+    currentUrl.pathname = basePath; // 设置新的路径
 
     // 跳转到新的URL
     window.location.href = currentUrl.href;
