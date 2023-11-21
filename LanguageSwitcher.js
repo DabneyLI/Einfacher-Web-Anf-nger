d02edff55b45eb38d4c9a2e1bcc94cfbe346e8d3
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-11-21 11:18:27 +0100

    language selector upating

--- LanguageSwitcher.js
+++ LanguageSwitcher.js
@@ -1,20 +1,17 @@
 document.getElementById('languageSelector').addEventListener('change', function () {
     var language = this.value;
     var currentUrl = new URL(window.location.href);
-    var currentPath = currentUrl.pathname; // 获取当前页面的路径
 
-    // 对于德语，我们需要移除子域名；对于其他语言，我们需要添加相应的子域名
-    var hostname = currentUrl.hostname;
-    var domain = 'einfachwebsiteerstellen.de';
-    var newSubdomain = language === 'de' ? '' : language + '.';
-    
-    // 如果当前已经是子域名，先去掉子域名部分
-    if (hostname.startsWith('en.') || hostname.startsWith('cn.')) {
-        hostname = hostname.substring(hostname.indexOf('.') + 1);
+    // 获取当前页面的路径，移除现有的语言子文件夹（如果存在）
+    var currentPath = currentUrl.pathname.replace(/\/(en|cn)\//, '/'); 
+
+    // 根据选择的语言更新路径
+    if (language === 'en' || language === 'cn') {
+        currentPath = '/' + language + currentPath;
     }
 
     // 构建新的URL
-    var newUrl = `https://${newSubdomain}${hostname}${currentPath}`;
+    var newUrl = currentUrl.origin + currentPath + currentUrl.search;
 
     // 跳转到新的URL
     window.location.href = newUrl;
